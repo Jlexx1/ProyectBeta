02d29ee558bfc1a3b1bafb1e183f12fa3a31fc6f
David Cobos <71732218+Madness2602@users.noreply.github.com>
2026-07-02 23:17:42 -0500

    delete

--- instalar_choco.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Instalando Chocolatey...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-echo.
-echo Hecho! Cerra y abri de nuevo la terminal.
-pause
